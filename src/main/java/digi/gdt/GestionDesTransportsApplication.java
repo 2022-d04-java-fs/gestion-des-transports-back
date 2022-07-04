7b81263ea1ed3b5088e86e47b71904078dcd656f
Emile DEBRIS <emile.gd@orange.fr>
2022-07-04 17:32:51 +0200

    init

---
+++ src/main/java/digi/gdt/GestionDesTransportsApplication.java
@@ -0,0 +1,13 @@
+package digi.gdt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionDesTransportsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDesTransportsApplication.class, args);
+	}
+
+}
