a21c21116915a718f92f9421b75d0f9d605e4d77
Rossi Oddet <rossi.oddet@dev-institut.fr>
2022-07-06 09:55:06 +0200

    err

--- src/main/java/digi/gdt/GestionDesTransportsApplication.java
+++ src/main/java/digi/gdt/GestionDesTransportsApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GestionDesTransportsApplication {
+public class GestionDesTransportsApplication 
 
 	public static void main(String[] args) {
 		SpringApplication.run(GestionDesTransportsApplication.class, args);
