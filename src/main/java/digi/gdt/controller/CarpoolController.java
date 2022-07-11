c0503b5d5a68e2a01076155d9c9d99e6113ba910
Axurynn <marion.bricout@outlook.com>
2022-07-11 16:20:43 +0200

    clean

--- src/main/java/digi/gdt/controller/CarpoolController.java
+++ src/main/java/digi/gdt/controller/CarpoolController.java
@@ -2,7 +2,6 @@ package digi.gdt.controller;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
