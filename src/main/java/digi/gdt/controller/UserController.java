5d4265aa74643f4261f5ecf01a53c59330800048
Jérémy PAYET <jpayet@diginamic-formation.fr>
2022-07-19 10:07:11 +0200

    init tests

--- src/main/java/digi/gdt/controller/UserController.java
+++ src/main/java/digi/gdt/controller/UserController.java
@@ -1,15 +1,17 @@
 package digi.gdt.controller;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
 import digi.gdt.dto.CreateUserDto;
+import digi.gdt.dto.UserDto;
 import digi.gdt.entity.Users;
 import digi.gdt.service.UserService;
 
@@ -24,7 +26,15 @@ public class UserController {
 		this.userSrv = userSrv;
 	}
 
-
+	/**
+	 * *GET* - Liste de tous les utilisateurs 'GET http://localhost:8080/api/users'
+	 * 
+	 * @return List<CreateUserDto>
+	 */
+	@GetMapping
+	public List<UserDto> listAll() {
+		return this.userSrv.findAll().stream().map(UserDto::from).toList();
+	}
 
 	@PostMapping
 	public ResponseEntity<?> createUser(@RequestBody CreateUserDto user) {
